d947c75a45a7f020dfbcef63a04999ea3af96fec
“rhlsnd” <sanadirahil@gmail.com>
2022-06-01 11:40:54 +0530

    this is my second commit

--- first.java
+++ first.java
@@ -1,5 +1,7 @@
 public class first{
     public static void main(String[] args){
         System.out.println("hello this is my first pgm");
+                System.out.println("hello this is my first pgm and line 2");
+
     }
 }
