7d7ba471e72457a85a2578e89f16e0ee51f19b20
“rhlsnd” <sanadirahil@gmail.com>
2022-06-01 11:50:30 +0530

    this is my third commit

--- first.java
+++ first.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class first{
     public static void main(String[] args){
         System.out.println("hello this is my first pgm");
                 System.out.println("hello this is my first pgm and line 2");
+                Scanner sc = new Scanner(System.in);
+                int a  =sc.nextInt();
+                System.out.println(a);
+                System.out.println(a+9);
+
 
     }
 }
