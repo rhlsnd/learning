f42063807ea048ca4e2ed79c6153dca6ff3ca57f
“rhlsnd” <sanadirahil@gmail.com>
2022-06-01 17:27:19 +0530

    this is second java pgm

---
+++ second.java
@@ -0,0 +1,11 @@
+import java.util.*;;
+public class second {
+    
+    public static void main(String[] args){
+        System.out.println("hello this is my second pgm");
+                System.out.println("hello this is my second pgm and line 2");
+            
+
+    }
+
+}
